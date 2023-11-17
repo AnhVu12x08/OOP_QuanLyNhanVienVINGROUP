59e1bfcc25643881649ae4cde1df27f18ca781ab
AnhVu12x08 <truonganhvu120804@gmail.com>
2023-11-17 19:09:53 +0700

    test b2 again

--- OOP_QuanLyNhanVienVINGROUP/testing1.cs
+++ OOP_QuanLyNhanVienVINGROUP/testing1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace OOP_QuanLyNhanVienVINGROUP
 {
     internal class testing1
     {
+        ChannelWriter<string> writer;
     }
 }
